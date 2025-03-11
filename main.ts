fdc2057a9d6387c03330376815730aadc8125800
adebola-io <adebolaakomolafe@gmail.com>
2025-03-11 23:36:21 +0100

    feat: Add commented out zero-js option

--- main.ts
+++ main.ts
@@ -5,3 +5,15 @@ import { createRouter } from './router';
 hydrate(createRouter).then(() => {
   console.log('[unfinished-ssg] app successfully hydrated.');
 });
+
+// -----------------------------------------------------------------------------
+// STATIC MODE. Will not add any client-side hydration.
+// -----------------------------------------------------------------------------
+// if (import.meta.env.DEV) {
+//   const { hydrate } = await import('@adbl/unfinished-ssg/client');
+//   const { createRouter } = await import('./router');
+
+//   const router = await hydrate(createRouter);
+//   await router.navigate(window.location.pathname);
+//   console.log('[unfinished-ssg] app successfully hydrated.');
+// }
